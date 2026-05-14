4c94dc25a6010aeb3c9ee627173c3b47a752b4f1
Corentin Hiver <corentin.hiver@gmail.com>
2026-05-14 13:47:51 +0200

    v1.2.1_p5.1 Caen2root: ADCmin initialized

--- caen2root.cpp
+++ caen2root.cpp
@@ -126,7 +126,7 @@ int main(int argc, char** argv)
   bool group = true;
   bool inMemory = true;
   bool applyCFD = true;
-  int ADCmin;
+  int ADCmin{};
   
   std::vector<std::string> filenames;
   Timeshifts timeshifts;
